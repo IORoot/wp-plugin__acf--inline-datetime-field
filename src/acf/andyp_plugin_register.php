3a464e8546da8d9e0187d5e552db9483b4ec9134
Andy Pearson <andy@londonparkour.com>
2020-12-26 13:34:15 +0000

    Reorganise

--- src/acf/andyp_plugin_register.php
+++ src/acf/andyp_plugin_register.php
@@ -5,7 +5,7 @@ add_action( 'plugins_loaded', function() {
     do_action('register_andyp_plugin', [
         'title'     => 'ACF Inline DateTime Picker',
         'icon'      => 'calendar-multiselect',
-        'color'     => '#607D8B',
+        'color'     => '#242424',
         'path'      => __FILE__,
     ]);
 } );
